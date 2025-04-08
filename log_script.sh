6c3d4ae5c6b08b3113f913e6b27659ba2b7e2599
alex <aikbox@yahoo.com>
2025-04-08 21:52:12 +0300

    Make script handle unset SHARED_LOG_PATH

--- log_script.sh
+++ log_script.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-LOG_DIR=$(SHARED_LOG_PATH)
+
+if [ -z "$SHARED_LOG_PATH" ]; then
+  LOG_DIR="/shared-logs"  # Default path if SHARED_LOG_PATH is not set
+else
+  LOG_DIR="$SHARED_LOG_PATH"
+fi
+
 mkdir -p "$LOG_DIR"
 date >> "$LOG_DIR/date.log"
 echo "---" >> "$LOG_DIR/date.log"
 cat "$LOG_DIR/date.log"
-#sleep 10
+# sleep 10
